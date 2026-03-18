5bf01299e1872015b6fa70d44899c15d80cf2e24
ashish <ashishmaurya089@gmail.com>
2026-03-18 14:58:06 +0530

    test

--- server/index.js
+++ server/index.js
@@ -59,6 +59,7 @@ app.post("/chat", async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000 🚀");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port} 🚀`);
 });
